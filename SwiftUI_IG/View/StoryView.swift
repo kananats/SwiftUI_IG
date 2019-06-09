a5091acde6d0d406335f5ef40fbd721583ee100d
Kananat Suwanviwatana <kananat.s@gmail.com>
2019-06-09 13:04:11 +0900

    make story view continuously spin

--- SwiftUI_IG/View/StoryView.swift
+++ SwiftUI_IG/View/StoryView.swift
@@ -12,6 +12,8 @@ extension Story {
     struct View: SwiftUI.View {
         let story: Story
         
+        @State private var dashPhase: Length = 0
+        
         var body: some SwiftUI.View {
             VStack {
                 Image(uiImage: story.content.image!.cropToSquare())
@@ -19,9 +21,15 @@ extension Story {
                     .clipShape(Circle())
                     .frame(width: 75, height: 75)
                     .clipped()
-                    .overlay(Circle().inset(by: -2.5).stroke(AngularGradient(gradient: Gradient(colors: Color.instagram), center: UnitPoint(x: 0.5, y: 0.5)), lineWidth: 5))
+                    .overlay(Circle().inset(by: -2.5).stroke(AngularGradient(gradient: Gradient(colors: Color.instagram), center: UnitPoint(x: 0.5, y: 0.5)), style: StrokeStyle(lineWidth: 5, dash: [7.5, 2.5], dashPhase: dashPhase)))
                     .overlay(Circle().inset(by: -1.25).stroke(Color.white, lineWidth: 2.5))
                     .shadow(radius: 4)
+                    .onAppear {
+                        withAnimation(Animation.basic(curve: .linear).repeatForever(autoreverses: false)) {
+                            self.dashPhase -= 18
+                        }
+                    }
+
                 Text(story.user.name)
                     .font(.caption)
             }
